be2a8d10f871c36eb1f4d295043124b56ca6442e
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-10-18 13:25:50 -0400

    feat: link, redirect test

--- src/Components/Pages/Products/Products.component.jsx
+++ src/Components/Pages/Products/Products.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const ProductsComponent = () => {
   return (
     <section>
       <h1>Products page</h1>
-
       <ul>
-        <li>Product No. 1 => Kamasutra book</li>
-        <li>Product No. 2 => BSDM book</li>
-        <li>Product No. 3 => Hentai book</li>
+        <li>
+          <Link to={'/product-detail/:p1'}>
+            Product No. 1 => Kamasutra book
+          </Link>
+        </li>
+        <li>
+          <Link to={'/product-detail/:p1'}>
+            Product No. 2 => BSDM book
+          </Link>
+        </li>
+        <li>
+          <Link to={'/product-detail/:p1'}>
+            Product No. 3 => Hentai book
+          </Link>
+        </li>
       </ul>
     </section>
   );
